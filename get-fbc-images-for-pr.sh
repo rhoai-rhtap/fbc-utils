0e2cd1bac2d41ba88d46611da395d4c70d7a226a
Alex Szczuczko <aszczucz@redhat.com>
2024-05-15 07:57:53 -0600

    Rework fbc-images-for-pr so that it waits for all checks to complete

--- get-fbc-images-for-pr.sh
+++ get-fbc-images-for-pr.sh
@@ -20,39 +20,73 @@ echo "=> Querying $repo/pull/$pr" >&2
 commit="$(call_gh "repos/$repo/pulls/$pr/commits" | jq -r '.[0] | .sha')"
 echo "=> Head commit ${commit:0:10}" >&2
 
-error=""
-while IFS=$'\t' read -r id status conclusion html_url details_url
+echo "=> Finding image coordinates" >&2
+while read -r image_coord
 do
-    if [[ "$status" == "completed" ]] && [[ "$conclusion" == "success" ]]
+    # Verify it exists
+    if skopeo inspect --no-tags "docker://$image_coord" >/dev/null
     then
-        # ex: https://console.redhat.com/preview/application-pipeline/ns/rhbk-release-tenant/pipelinerun/rhbk-fbc-component-v4-12-on-pull-request-4rpfs
-        IFS=$'\t' read -r konflux_namespace konflux_component < <(sed -r 's|^.*/application-pipeline/ns/([^/]+)/pipelinerun/([^/]+)-on-pull-request[^/]*$|\1\t\2|;tx;d;:x' <<<"$details_url")
-
-        konflux_application="$(sed 's/-component//' <<<"$konflux_component")"
-
-        # ex: quay.io/redhat-user-workloads/rhbk-release-tenant/rhbk-fbc-v4-13/rhbk-fbc-component-v4-13:on-pr-51476f518900c699e66c4d0fdd33dbb9524e83de
-        image_coord="quay.io/redhat-user-workloads/$konflux_namespace/$konflux_application/$konflux_component:on-pr-$commit"
-
-        # Verify it exists
-        skopeo inspect --no-tags "docker://$image_coord" >/dev/null
-
         echo "$image_coord"
     else
-        echo "$id is $status ($conclusion), see $html_url" >&2
-        error="yes"
+        echo "WARN: found image coord does not exist: $image_coord" >&2
     fi
 done < <(
-    call_gh "repos/$repo/commits/$commit/check-runs" \
-        | jq -r '.check_runs
-            | .[]
-            | select(.external_id | contains("-on-pull-request-"))
-            | [.external_id, .status, .conclusion, .html_url, .details_url]
-            | @tsv' \
-        | sort -V
-)
+    completed_check_ids=()
+    while true
+    do
+        waiting=""
+        while IFS=$'\t' read -r id status conclusion html_url details_url
+        do
+            # Don't repeatedly show checks that we've already shown as pass/fail
+            skip=""
+            for completed_id in "${completed_check_ids[@]}"
+            do
+                if [[ "$completed_id" == "$id" ]]
+                then
+                    skip="yes"
+                fi
+            done
+            if [ -n "$skip" ]
+            then
+                continue
+            fi
 
-if [ -n "$error" ]
-then
-    echo "ERROR: one or more checks have not completed successfully"
-    exit 10
-fi
+            if [[ "$status" == "completed" ]] && [[ "$conclusion" == "success" ]]
+            then
+                echo " -> $id has $status ok" >&2
+                completed_check_ids+=("$id")
+                # ex: https://console.redhat.com/preview/application-pipeline/ns/rhbk-release-tenant/pipelinerun/rhbk-fbc-component-v4-12-on-pull-request-4rpfs
+                IFS=$'\t' read -r konflux_namespace konflux_component < <(sed -r 's|^.*/application-pipeline/ns/([^/]+)/pipelinerun/([^/]+)-on-pull-request[^/]*$|\1\t\2|;tx;d;:x' <<<"$details_url")
+
+                konflux_application="$(sed 's/-component//' <<<"$konflux_component")"
+
+                # ex: quay.io/redhat-user-workloads/rhbk-release-tenant/rhbk-fbc-v4-13/rhbk-fbc-component-v4-13:on-pr-51476f518900c699e66c4d0fdd33dbb9524e83de
+                image_coord="quay.io/redhat-user-workloads/$konflux_namespace/$konflux_application/$konflux_component:on-pr-$commit"
+
+                echo "$image_coord"
+            elif [[ "$status" != "completed" ]]
+            then
+                echo " -> $id is $status" >&2
+                waiting="yes"
+            else
+                echo "ERROR: $id is $status ($conclusion), see $html_url" >&2
+                completed_check_ids+=("$id")
+            fi
+        done < <(
+            call_gh "repos/$repo/commits/$commit/check-runs" \
+                | jq -r '.check_runs
+                    | .[]
+                    | select(.external_id | contains("-on-pull-request-"))
+                    | [.external_id, .status, .conclusion, .html_url, .details_url]
+                    | @tsv' \
+                | sort -V
+        )
+        if [ -z "$waiting" ]
+        then
+            break
+        else
+            echo "=> Waiting for checks to complete..." >&2
+            sleep 60s
+        fi
+    done
+)
